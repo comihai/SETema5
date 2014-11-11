2fe85ab2f37a3925fe5dd62357a51054bd992907
comihai <coca.mihai5@gmail.com>
2014-11-11 20:14:41 +0200

    added IProcessor interface for ProcessorFactory

---
+++ src/com/packet/interfaces/IProcessor.java
@@ -0,0 +1,11 @@
+package com.packet.interfaces;
+
+/**
+ * Created by mihai on 11/11/2014.
+ * This is the interface for ProcessorFactory
+ */
+public interface IProcessor {
+
+    void clockSpeed();
+
+}
