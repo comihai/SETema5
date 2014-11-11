1992932041c24ca15077bbba15960b5532bc332b
comihai <coca.mihai5@gmail.com>
2014-11-11 19:54:32 +0200

    added Embedded class - concrete object for OperatingSystemFactory

---
+++ src/com/packet/operatingSystems/Embedded.java
@@ -0,0 +1,28 @@
+package com.packet.operatingSystems;
+
+import com.packet.interfaces.IOperatingSystem;
+
+/**
+ * Created by mihai on 11/11/2014.
+ * This is the fourth concrete object
+ */
+public class Embedded implements IOperatingSystem {
+    /**
+     * Private Member
+     */
+    private double autonomy = 0;
+
+    /**
+     * Generic constructor
+     *
+     * @param autonomy Autonomy used for operating with a limited number of resources
+     */
+    public Embedded(double autonomy) {
+        this.autonomy = autonomy;
+    }
+
+    @Override
+    public void systemCall() {
+        System.out.println("This operating system is designed to be used in embedded computer systems! Autonomy :  " + autonomy);
+    }
+}
