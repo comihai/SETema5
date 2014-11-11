70ca189fb6c4ca705ea89b13f05811685390f9c1
comihai <coca.mihai5@gmail.com>
2014-11-11 19:27:01 +0200

    added interface IOperatingSystem for OperatingSystemFactory

--- src/com/packet/Main.java
+++ src/com/packet/Main.java
@@ -2,13 +2,11 @@ package com.packet;
 
 /**
  * Created by mihai on 11/10/2014.
- *
+ * <p/>
  * This is the main class that holds the role of the client
- *
  */
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         //create factory instances
     }
 }
---
+++ src/com/packet/interfaces/IOperatingSystem.java
@@ -0,0 +1,10 @@
+package com.packet.interfaces;
+
+/**
+ * Created by mihai on 11/10/2014.
+ * <p/>
+ * This is the interface for OperatingSystemFactory
+ */
+public interface IOperatingSystem {
+    void systemCall();
+}
